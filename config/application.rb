5e8bca4c9e327a9dc6c74cd04ff569326467cb84
wkojiro1 <wkojiro@eazydrive.co.jp>
2016-01-07 09:45:24 +0000

    css fuck

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module DriverCareer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    #今回は、テストコードとJavaScript、CSS(SCSS)、ヘルパのファイルを自動生成しないので、Rails側であらかじめ設定しておきます
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end    
+    
+    
   end
 end
